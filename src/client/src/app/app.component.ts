9c22927b51864fa917540d4f57857c3bdf95d897
Marcin Miazga <miazgamarcin@outlook.com>
2019-01-02 13:00:17 +0100

    refactor

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { HubConnection } from '@aspnet/signalr';
-
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
 import { Message } from 'primeng/api';
-import * as signalR from '@aspnet/signalr';
-
-import webNotification from 'simple-web-notification'
+import webNotification from 'simple-web-notification';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   private connection: HubConnection;
   private messages: Message[] = [];
 
-  constructor() { }
-
   ngOnInit(): void {
-    this.connection = new signalR.HubConnectionBuilder().withUrl('https://signalr-demo-dotnetgroup.azurewebsites.net/notifications').build();
+    this.connection = new HubConnectionBuilder()
+      .withUrl('https://signalr-demo-dotnetgroup.azurewebsites.net/notifications')
+      .build();
     this.connection
       .start()
       .then(() => console.log('Connection started!'))
@@ -31,7 +29,7 @@ export class AppComponent implements OnInit {
     // this.connection.on('BroadcastMessage', (type: string, payload: string) => {
     //   webNotification.showNotification(type, {
     //     body: payload,
-    //     onClick: () => {console.log("clicked")},
+    //     onClick: () => { console.log('clicked!'); },
     //     autoClose: 7000
     //   });
     // });
